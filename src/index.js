ed93cf5479f0fe22ec7baeb2c7a07d427a899a19
Schuyler <skym1n0r91@gmail.com>
2022-01-11 17:10:04 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 import './less/index.less'
 
 // Your code goes here!
+
+const navLinks = document.querySelector('nav');
+navLinks.addEventListener("mouseover", function(event) {
+    event.target.style.color = "skyblue";
+    setTimeout(function() {
+        event.target.style.color = "";
+    }, 500);
+}, false);
+
+navLinks.onclick = function(event) {
+    console.log("Lets Click IT!!!")
+}
+
+// const signUpBtn = document.querySelector('.btn');
+
+
+
+// signUpBtn.ondblclick = function(event) {
+//     console.log("Clickin' the habit!!!")
+// }
+
+Array.from(document.querySelectorAll('.btn')).forEach(elem => elem.addEventListener('click', evt => {
+    console.log('clickin the habit!!')
+}))
